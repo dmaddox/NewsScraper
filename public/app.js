ede1c60316887d5975836fbdb7502aaee31c9014
dmaddox <dmaddox@gmail.com>
2018-01-17 18:25:36 -0600

    setup app.js and index.html, and initial layout of scraped content

---
+++ public/app.js
@@ -0,0 +1,10 @@
+// Grab the articles as a json
+$.getJSON("/articles", function(data) {
+  // For each one
+  for (var i = 0; i < data.length; i++) {
+    // Display the apropos information on the page
+    $("#articles").append("<p data-id='" + data[i]._id + "'><a href=\"" + data[i].link + "\" class=\"title\">" + data[i].title + "</a><br />" + 
+      data[i].excerpt + "</p>");
+  }
+});
+
